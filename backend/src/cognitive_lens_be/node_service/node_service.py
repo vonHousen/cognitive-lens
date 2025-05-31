2f70ae46de2974354ce971024d73f44e8b77115d
Kamil Zych <42911623+vonHousen@users.noreply.github.com>
2025-05-31 16:54:43 -0700

    Pass 'output_schema' to the LLM.

--- backend/src/cognitive_lens_be/node_service/node_service.py
+++ backend/src/cognitive_lens_be/node_service/node_service.py
@@ -1,10 +1,11 @@
 from typing import Any
 
-from litellm import acompletion
+from litellm import acompletion, BadRequestError
 
 from cognitive_lens_be.model.conversation_message import ConversationMessage
 from cognitive_lens_be.model.message import Message, AgentRole
 from cognitive_lens_be.model.result_messages import ResultMessages
+from cognitive_lens_be.utils.logger import LOGGER
 
 
 class NodeService:
@@ -21,10 +22,16 @@ class NodeService:
         executor_input_messages = [{"content": system_prompt, "role": "system"}] if system_prompt else []
         executor_input_messages.extend([msg.model_dump() for msg in conversation])
 
-        executor_initial_response = await acompletion(
-            model="gpt-4.1-mini",
-            messages=executor_input_messages,
-        )
+        try:
+            executor_initial_response = await acompletion(
+                model="gpt-4.1-mini",
+                messages=executor_input_messages,
+                response_format=output_schema or None,
+            )
+        except BadRequestError:
+            LOGGER.exception("Error while LLM requesting.")
+            raise
+
         executor_msg = executor_initial_response.choices[0].message.content
 
         # TODO add feedback loop
